e4902a118d283c89bc7516c605a94583a8e7b4d8
Elior Mangistu <123584426+eliormangistu@users.noreply.github.com>
2025-11-27 17:38:38 +0200

    Update dev.js

--- config/dev.js
+++ config/dev.js
@@ -1,4 +1,4 @@
 export default {
-  dbURL: 'mongodb+srv://elior:elior123@cluster0.abmony7.mongodb.net/?retryWrites=true&w=majority',
+  dbURL: 'mongodb+srv://elior:elior1234@cluster0.abmony7.mongodb.net/?retryWrites=true&w=majority',
   dbName: 'story_db'
 }
